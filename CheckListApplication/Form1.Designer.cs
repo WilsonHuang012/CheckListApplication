bcb7216147bbe443494cb3e28b9a70a1edc22f36
Wilson Huang <window012@hotmail.com>
2021-04-22 21:28:29 +0800

    Add project files.

---
+++ CheckListApplication/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace CheckListApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.ItemTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.checkedListBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1066, 972);
+            this.splitContainer1.SplitterDistance = 300;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox1.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(300, 972);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.ItemTextBox);
+            this.splitContainer2.Panel1.Controls.Add(this.SaveButton);
+            this.splitContainer2.Panel1.Controls.Add(this.DeleteButton);
+            this.splitContainer2.Panel1.Controls.Add(this.AddButton);
+            this.splitContainer2.Size = new System.Drawing.Size(762, 972);
+            this.splitContainer2.SplitterDistance = 400;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(22, 243);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(221, 66);
+            this.SaveButton.TabIndex = 2;
+            this.SaveButton.Text = "儲存";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(22, 139);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(221, 66);
+            this.DeleteButton.TabIndex = 1;
+            this.DeleteButton.Text = "刪除項目";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(22, 36);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(221, 66);
+            this.AddButton.TabIndex = 0;
+            this.AddButton.Text = "新增項目";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // ItemTextBox
+            // 
+            this.ItemTextBox.Font = new System.Drawing.Font("PMingLiU", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ItemTextBox.Location = new System.Drawing.Point(290, 39);
+            this.ItemTextBox.Name = "ItemTextBox";
+            this.ItemTextBox.Size = new System.Drawing.Size(374, 59);
+            this.ItemTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 972);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.TextBox ItemTextBox;
+    }
+}
+
---
+++ CheckListApplication/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.Json;
+
+namespace CheckListApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            checkedListBox1.Items.Clear();
+            LoadFile();
+        }
+
+        const string fileName = "list.dat";
+
+        private void LoadFile()
+        {
+            if (File.Exists(fileName))
+            {
+                Dictionary<string, bool> list = new Dictionary<string, bool>();
+                using (FileStream fs = new FileStream(fileName, FileMode.Open))
+                {
+                    using (BinaryReader reader = new BinaryReader(fs))
+                    {
+                        while (fs.Length != fs.Position)
+                        {
+                            string json = reader.ReadString();
+                            list = JsonSerializer.Deserialize<Dictionary<string, bool>>(json);
+                        }
+                    }
+                }
+                foreach (var item in list)
+                {
+                    checkedListBox1.Items.Add(item.Key, item.Value == true);
+                }
+            }
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ItemTextBox.Text))
+            {
+                MessageBox.Show("未輸入項目!", "訊息", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                checkedListBox1.Items.Add(ItemTextBox.Text);
+                ItemTextBox.Text = string.Empty;
+            }
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.Items.Count != 0)
+            {
+                if (checkedListBox1.SelectedItem == null)
+                {
+                    MessageBox.Show("未選擇項目!", "訊息", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else
+                {
+                    checkedListBox1.Items.RemoveAt(checkedListBox1.SelectedIndex);
+                } 
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, bool> list = new Dictionary<string, bool>();
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                string name = checkedListBox1.Items[i].ToString();
+                bool value = checkedListBox1.GetItemChecked(i);
+                list.Add(name, value);
+            }
+
+            if (File.Exists(fileName) || list.Count != 0)
+            {
+                SaveFile(list);
+            }
+        }
+
+        private void SaveFile(Dictionary<string, bool> list)
+        {
+            var json = JsonSerializer.Serialize(list);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+            {
+                writer.Write(json);
+            }
+            MessageBox.Show("儲存成功", "訊息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+    }
+}
